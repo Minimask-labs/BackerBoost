652826d02089dcd7cae090a6b3eb7935367df8f7
Borngreat Ikwutah <iagocech2056@gmail.com>
2025-05-16 18:20:07 +0100

    feat: added main backberboost structure...

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,27 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  typedRoutes: true,
   experimental: {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
+  },
+  output: "standalone",
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+      topLevelAwait: true,
+      layers: true,
+    };
+    if (!isServer) {
+      config.output.environment = {
+        ...config.output.environment,
+        asyncFunction: true,
+      };
+    }
+    return config;
   },
 }
 
